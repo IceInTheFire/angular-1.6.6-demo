d7dc114103993e1d7a224c58029b9a950a5338f4
IceInTheFire <392587201@qq.com>
2017-12-26 16:42:44 +0800

    fix

--- js/core.js
+++ js/core.js
@@ -2436,34 +2436,6 @@
         }
     }
 })();
-(function () {
-    angular
-        .module('app')
-        .controller('dialog.confirm', ['$scope', 'Core', '$uibModalInstance','params',HomeController]);
-
-    function HomeController($scope, Core, $uibModalInstance,params){
-
-        var context = $scope;
-
-        context.onClickOk = onClickOk;
-        context.onClickCancel= onClickCancel;
-
-        init();
-        function init() {
-            context.params = params;
-            context.params.header = context.params.header || '确认框';
-            // context.loading = true;
-        }
-
-        function onClickOk() {
-            $uibModalInstance.close('确认');
-        }
-
-        function onClickCancel() {
-            $uibModalInstance.dismiss('取消');
-        }
-    }
-}());
 (function() {
     var app = angular.module('app',[
         'ui.router',
@@ -2820,3 +2792,31 @@
         $rootScope.$state = $state;
     }
 })();
+(function () {
+    angular
+        .module('app')
+        .controller('dialog.confirm', ['$scope', 'Core', '$uibModalInstance','params',HomeController]);
+
+    function HomeController($scope, Core, $uibModalInstance,params){
+
+        var context = $scope;
+
+        context.onClickOk = onClickOk;
+        context.onClickCancel= onClickCancel;
+
+        init();
+        function init() {
+            context.params = params;
+            context.params.header = context.params.header || '确认框';
+            // context.loading = true;
+        }
+
+        function onClickOk() {
+            $uibModalInstance.close('确认');
+        }
+
+        function onClickCancel() {
+            $uibModalInstance.dismiss('取消');
+        }
+    }
+}());
